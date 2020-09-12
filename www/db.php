0fa9a7d060d711d01f41e1956d3821fd24cb1adc
Oliver Leitner <never@neverslair-blog.net>
2020-09-12 16:13:38 +0200

    sec check on gameID

--- www/db.php
+++ www/db.php
@@ -10,6 +10,6 @@ $record["gameTimeEastern"] = $dt->getTimeStamp();
 $record["homeID"] = $_GET["homeID"];
 $record["visitorID"] = $_GET["visitorID"];
 if (!$_GET['gameID']) $db->autoExecute($table,$record,'INSERT');
-else $db->autoExecute($table,$record,'UPDATE', 'gameID = '.$_GET['gameID']);
+else $db->autoExecute($table,$record,'UPDATE', 'gameID = '.intval($_GET['gameID']));
 $status = array("status" => "success");
 echo json_encode($status);
