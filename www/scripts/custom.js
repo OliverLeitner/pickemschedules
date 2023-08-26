3fc51a419095c761847591a2751991169c7ece9a
Oliver Leitner <never@neverslair-blog.net>
2023-08-26 13:34:17 +0200

    dropdown refill fixed, code reduction, better logger functionality

--- www/scripts/custom.js
+++ www/scripts/custom.js
@@ -1,11 +1,11 @@
 "use strict";
 
-function logHelper() {
+function logHelper(data = undefined) {
+    !data ? null : console.log(data);
     console.log("weekNum: " + document.getElementsByName('weekNum')[0].value);
     console.log("gameTimeEastern: " + document.getElementsByName('gameTimeEastern')[0].value);
     console.log("homeID: " + document.getElementsByName('homeID')[0].value);
     console.log("visitorID: " + document.getElementsByName('visitorID')[0].value);
-
     console.log("save button: " + document.getElementsByName('save')[0].disabled);
 }
 
@@ -41,12 +41,15 @@ function fillDataToForm(data) {
     document.getElementsByName('gameID')[0].value = data.fields['gameID'];
     document.getElementsByName('weekNum')[0].value = data.fields['weekNum'];
     document.getElementsByName('gameTimeEastern')[0].value = data.fields['gameTimeEastern'];
-    document.getElementsByName('homeID')[0].value = data.fields['homeID'];
-    document.getElementsByName('visitorID')[0].value = data.fields['visitorID'];
+    changeValue(home,data.fields['homeID']);
+    changeValue(visitor,data.fields['visitorID']);
+}
+
+function changeValue(field,val) {
+    field.setValue(val);
 }
 
 function checkRequiredFields() {
-    // logHelper(); // debug function
     if (
         document.getElementsByName('weekNum')[0].value > 0 &&
         document.getElementsByName('gameTimeEastern')[0].value !== '' &&
@@ -55,4 +58,45 @@ function checkRequiredFields() {
     ) document.getElementsByName('save')[0].disabled = false;
     else
         document.getElementsByName('save')[0].disabled = true;
+}
+
+function loadCal(id) {
+        return new tempusDominus.TempusDominus(document.getElementById(id),
+             { display: {
+                     icons: {
+                         type: 'icons',
+                         time: 'fa fa-clock-o',
+                         date: 'fa fa-calendar',
+                         up: 'fa fa-arrow-up',
+                         down: 'fa fa-arrow-down',
+                         previous: 'fa fa-chevron-left',
+                         next: 'fa fa-chevron-right',
+                         today: 'fa fa-calendar-check',
+                         clear: 'fa fa-trash',
+                         close: 'fa fa-xmark'
+                     },
+                     sideBySide: true,
+                     components: {
+                         calendar: true,
+                         date: true,
+                         month: true,
+                         year: true,
+                         decades: false,
+                         clock: true,
+                         hours: true,
+                         minutes: true,
+                         seconds: false,
+                     },
+                 },
+                 localization: {
+                     dateFormats: {
+                         L: 'yyyy-MM-dd H:mm'
+                     },
+                     format: 'L'
+                 },
+                 restrictions: {
+                     minDate: new Date(),
+                     maxDate: new Date(new Date().getFullYear() + 1, new Date().getMonth(), new Date().getDate())
+                 }
+             });
 }
